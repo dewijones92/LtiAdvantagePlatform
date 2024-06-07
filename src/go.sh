d63ae9b10c26e1584b57a1dfab7389cc88d8ff96
Dewi Jones <dewijones92@gmail.com>
2024-06-07 08:08:44 +0100

    more

--- src/go.sh
+++ src/go.sh
@@ -4,4 +4,4 @@ set -e
 git clean -xdf
 #docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>" -p 1433:1433 --name sql1 --hostname sql1 -d mcr.microsoft.com/mssql/server:2022-latest
 
-exec ub docker run --name lti-platform --rm --network host -v $(pwd):/app -w /app mcr.microsoft.com/dotnet/core/sdk:2.2 /bin/bash -c "dotnet clean && dotnet build && dotnet run" |& ub tsm  |& ub tee /home/ltiuser/logs/logs/dotnet_logs
+exec ub docker run --name lti-platform --rm --network host -v $(pwd):/app -w /app mcr.microsoft.com/dotnet/core/sdk:2.2 /bin/bash -c "dotnet clean && dotnet build && dotnet run" |& ub tsm  |& ub tee -a /home/ltiuser/logs/logs/dotnet_logs
